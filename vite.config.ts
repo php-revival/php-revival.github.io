9ebb63e381e03c1d69b6d9401b3655006650911f
SerhiiCho <serhiicho@protonmail.com>
2024-04-24 19:46:34 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/',
+
     plugins: [vue()],
 
     resolve: {
         alias: {
             '@': '/src',
         },
+    },
+
+    server: {
+        port: 3000,
     },
 })
