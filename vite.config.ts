029eee11bcf6cded102cfaf4343bc6d4344f762f
SerhiiCho <serhiicho@protonmail.com>
2024-04-24 20:04:05 +0300

    Fix?

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
+    base: process.env.NODE_ENV === 'production' ? '/dist' : '/',
 
     plugins: [vue()],
 
