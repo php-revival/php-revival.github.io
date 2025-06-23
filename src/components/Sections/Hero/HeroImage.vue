492708ffa5725b339df2d2ba546c22082cf820ed
SerhiiCho <serhiicho@protonmail.com>
2025-06-23 23:17:08 +0300

    chore: remove animation

--- src/components/Sections/Hero/HeroImage.vue
+++ src/components/Sections/Hero/HeroImage.vue
@@ -9,26 +9,8 @@ import Parallaxy from '@lucien144/vue3-parallaxy'
                 src="@/assets/example.png"
                 width="1100"
                 height="577"
-                class="mx-auto mt-24 md:mt-32 lg:mt-36 rounded-xl _hero-image"
+                class="mx-auto mt-24 md:mt-32 lg:mt-36 rounded-xl"
             />
         </Parallaxy>
     </div>
 </template>
-
-<style scoped>
-._hero-image {
-    animation: shadow-animation 2s infinite;
-}
-
-@keyframes shadow-animation {
-    0% {
-        box-shadow: 0 -25px 70px rgba(120,175,230,.5);
-    }
-    50% {
-        box-shadow: 0 -15px 50px rgba(120, 199, 230, 0.5);
-    }
-    100% {
-        box-shadow: 0 -25px 70px rgba(120,175,230,.5);
-    }
-}
-</style>
